8f7e399f280bc7a153f3ec02701a20d3f5eee49a
Christopher Ehlen <googleapps@iamnear.com>
2016-03-21 18:27:25 -0700

    Change set receiver to self

--- typeSet.go
+++ typeSet.go
@@ -10,17 +10,17 @@ func newTypeSet() *typeSet {
 	return &typeSet{make(map[reflect.Type]bool)}
 }
 
-func (set *typeSet) add(i reflect.Type) bool {
-	_, found := set.set[i]
-	set.set[i] = true
+func (self *typeSet) add(i reflect.Type) bool {
+	_, found := self.set[i]
+	self.set[i] = true
 	return !found
 }
 
-func (set *typeSet) get(i reflect.Type) bool {
-	_, found := set.set[i]
+func (self *typeSet) get(i reflect.Type) bool {
+	_, found := self.set[i]
 	return found
 }
 
-func (set *typeSet) remove(i reflect.Type) {
-	delete(set.set, i)
+func (self *typeSet) remove(i reflect.Type) {
+	delete(self.set, i)
 }
